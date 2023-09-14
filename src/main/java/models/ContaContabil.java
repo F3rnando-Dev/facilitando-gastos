c21d48100e98ca278814b119e354955a3fcfdbbf
Fernando Santana <fernando_92-@hotmail.com>
2023-09-14 16:01:46 -0300

    feat: add model base das contas contabeis

---
+++ src/main/java/models/ContaContabil.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_CONTACONTABIL")
+public class ContaContabil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Getter
+    @Setter
+    private String descricao;
+
+    
+
+}
