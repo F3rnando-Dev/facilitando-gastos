42edef11269c077ff615aa71ee850f345866434c
Fernando Santana <fernando_92-@hotmail.com>
2023-09-14 15:30:20 -0300

    start: commit inicial do projeto

---
+++ src/test/java/com/facilitandogastos/facilitandogastos/FacilitandoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facilitandogastos.facilitandogastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitandoGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
