337329052156b93da7d3b573069c73e90f4d1cab
Fernando Santana <fernando_92-@hotmail.com>
2023-09-15 19:14:06 -0300

    restart: iniciando projeto do zero

---
+++ src/main/java/com/facilitandogastos/project/FacilitandoGastosApplication.java
@@ -0,0 +1,13 @@
+package com.facilitandogastos.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitandoGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitandoGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facilitandogastos/project/FacilitandoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facilitandogastos.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitandoGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
