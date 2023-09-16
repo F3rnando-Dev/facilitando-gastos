bf022e480915a3cdd646391b4f3eaa25864ac7f7
Fernando Santana <fernando_92-@hotmail.com>
2023-09-16 10:22:35 -0300

    feat: add metodo get all para despesas

--- src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
+++ src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
@@ -8,9 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DespesaController {
@@ -26,5 +29,12 @@ public class DespesaController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(despesaRepository.save(despesaModel));
     }
+
+    @GetMapping("/despesas")
+    public ResponseEntity<List<DespesaModel>> getAllDespesas(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(despesaRepository.findAll());
+    }
+
 
 }
