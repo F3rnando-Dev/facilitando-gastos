2461ec4c8d11214363124ef417c36e5d3a7179ca
Fernando Santana <fernando_92-@hotmail.com>
2023-09-16 11:49:55 -0300

    feat: crud para despesas

--- src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
+++ src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
@@ -8,12 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 public class DespesaController {
@@ -35,6 +34,48 @@ public class DespesaController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(despesaRepository.findAll());
     }
+
+    @GetMapping("/despesas/{id}")
+    public ResponseEntity<Object> getOneDespesa(@PathVariable(value = "id") Long id){
+        Optional<DespesaModel> despesaOpt = despesaRepository.findById(id);
+        if (despesaOpt.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Despesa não encontrada");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(despesaOpt.get());
+    }
+
+    @PutMapping("/despesas/{id}")
+    public ResponseEntity<Object> updateDespesa(@PathVariable(value = "id") Long id,
+                                                @RequestBody @Valid DespesaRecordDto despesaRecordDto){
+        Optional<DespesaModel> despesaOpt = despesaRepository.findById(id);
+        if (despesaOpt.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Despesa não encontrada");
+        }
+
+        var despesaModel = despesaOpt.get();
+        BeanUtils.copyProperties(despesaRecordDto, despesaModel);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(despesaRepository.save(despesaModel));
+    }
+
+    @DeleteMapping("/despesas/{id}")
+    public ResponseEntity<Object> deleteDespesa(@PathVariable(value = "id") Long id){
+        Optional<DespesaModel> despesaOpt = despesaRepository.findById(id);
+        if (despesaOpt.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Despesa não encontrada!");
+        }
+
+        despesaRepository.delete(despesaOpt.get());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Despesa deletada com sucesso!");
+    }
+
 
 
 }
