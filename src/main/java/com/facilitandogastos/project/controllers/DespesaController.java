daa09fd1e036d86ebfc2525f4bb68e56fb92a003
Fernando Santana <fernando_92-@hotmail.com>
2023-09-15 23:29:05 -0300

    feat: add metodo post para despesas

--- src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
+++ src/main/java/com/facilitandogastos/project/controllers/DespesaController.java
@@ -1,7 +1,15 @@
 package com.facilitandogastos.project.controllers;
 
+import com.facilitandogastos.project.dtos.DespesaRecordDto;
+import com.facilitandogastos.project.models.DespesaModel;
 import com.facilitandogastos.project.repositories.DespesaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +18,13 @@ public class DespesaController {
     @Autowired
     DespesaRepository despesaRepository;
 
+    @PostMapping("/despesas")
+    public ResponseEntity<DespesaModel> saveDespesa(@RequestBody @Valid DespesaRecordDto despesaRecordDto){
+        var despesaModel = new DespesaModel();
 
+        BeanUtils.copyProperties(despesaRecordDto, despesaModel);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(despesaRepository.save(despesaModel));
+    }
 
 }
